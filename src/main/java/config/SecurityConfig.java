2613506734e155d6ab8585c54f30d2f5e0b4fd27
Marz <mohammadhashmi@graduate.utm.my>
2024-01-14 15:27:15 +0800

    Revert "FUCKING FINNALY"
    This reverts commit b146c2c77861309b4b6c14a84de62444d6d2697d.

--- src/main/java/config/SecurityConfig.java
+++ src/main/java/config/SecurityConfig.java
@@ -11,44 +11,48 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	// this class create the spring security filter chain for our app
-	// donï¿½t put any code at this moment
-
+//this class create the spring security filter chain for our app
+	// don’t put any code at this moment
+	
 	@Autowired
 	DataSource dataSource;
-
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-		auth.jdbcAuthentication()
-				.dataSource(this.dataSource)
-				.usersByUsernameQuery("SELECT email,password,enabled FROM users WHERE email = ?")
-				.authoritiesByUsernameQuery("SELECT email,authority FROM authorities WHERE email = ?")
-				.passwordEncoder(new BCryptPasswordEncoder());
-
+			auth.jdbcAuthentication()
+			.dataSource(this.dataSource)
+		    .usersByUsernameQuery("SELECT email,password,enabled FROM users WHERE email = ?")
+		    .authoritiesByUsernameQuery("SELECT email,authority FROM authorities WHERE email = ?")
+			.passwordEncoder(new BCryptPasswordEncoder());
+		
 	}
-
+	
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	protected void configure(HttpSecurity http) throws Exception{
 
 		http.formLogin(form -> form.loginPage("/login").permitAll());
 		// This is used to add custom login page
 		http
-				.authorizeRequests()
-				.antMatchers("/resources/**")
-				.permitAll()
-				.antMatchers("/")
-				.permitAll()
-				.antMatchers("/register/**")
-				.permitAll()
-				.anyRequest().authenticated()
-				.and()
-				.logout()
-				.permitAll()
-				.and().csrf().disable();
+		.authorizeRequests()
+		.antMatchers("/resources/**")
+		.permitAll()
+		.antMatchers("/")
+		.permitAll()
+		.antMatchers("/register/**")
+		.permitAll()
+		.anyRequest().authenticated()
+		.and()
+		.logout()
+		.permitAll()
+		.and().csrf().disable()
+		; 
+		
+		
 
 	}
 
